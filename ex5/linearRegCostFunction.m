ac2cdecedcf1e202d4ce6743e24d7b3b3bb7a22f
szypul <szypull@gmail.com>
2018-05-11 22:09:21 +0200

    ex 2.1

--- ex5/linearRegCostFunction.m
+++ ex5/linearRegCostFunction.m
@@ -26,7 +26,6 @@ grad = zeros(size(theta));
 h = X*theta;
 
 J  =  (1/(2*m)) * ((h - y)'*(h-y)) +  (lambda/(2*m)) * ((theta(2:end)')*theta(2:end));
-% + (lambda/(2*m)) * ((theta(2:end)')*theta(2:end));;
 
 grad = 1/m* X' * (h-y) + (lambda/m)*[0; theta(2:end)];;
 
